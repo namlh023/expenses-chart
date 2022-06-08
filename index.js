6ae46a90d7f5ee40aba62011c1aadb0fe641f0c4
namlh <hoainamle023@gmail.com>
2022-06-08 07:50:35 +0700

    feat: chart completed

--- index.js
+++ index.js
@@ -23,17 +23,21 @@ for (let item of data) {
   let li = document.createElement("li");
   let div = document.createElement("div");
   let p = document.createElement("p");
+  let p2 = document.createElement("p");
 
   li.className = "chart__item";
   div.className = "chart__column";
   p.className = "chart__label";
+  p2.className = "chart__number";
 
   p.innerText = item.day;
+  p2.innerHTML = `$ ${item.amount}`;
   div.style.height = `${getHeight(item.amount, max)}px`;
   if (item.amount == max) {
     div.classList.add("chart__column--tallest");
   }
 
+  li.append(p2);
   li.append(div);
   li.append(p);
   chart.append(li);
